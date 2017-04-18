cb5f619befe1093d3cb06aa416b1d3a49ac82771
Sam Skillman <samskillman@gmail.com>
2017-04-18 11:48:34 -0600

    Update versions

--- descarteslabs/__init__.py
+++ descarteslabs/__init__.py
@@ -14,7 +14,7 @@
 
 # flake8: noqa
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 from .auth import Auth
 descartes_auth = Auth()
 
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import os
 from setuptools import setup, find_packages
 
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 def do_setup():
