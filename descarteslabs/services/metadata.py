711ddbbf36792c31e9808384b4b99b1b5a2ee64e
Justin Poehnelt <justin.poehnelt@gmail.com>
2017-06-20 17:41:08 -0600

    add id to doctest keys

--- descarteslabs/services/metadata.py
+++ descarteslabs/services/metadata.py
@@ -378,9 +378,10 @@ class Metadata(Service):
             >>> keys
             ['acquired', 'area', 'bits_per_pixel', 'bright_fraction', 'bucket', 'cloud_fraction',
              'cloud_fraction_0', 'cs_code', 'descartes_version', 'file_md5s', 'file_sizes', 'files',
-             'fill_fraction', 'geolocation_accuracy', 'geometry', 'geotrans', 'identifier', 'key', 'processed',
-             'product', 'projcs', 'published', 'raster_size', 'reflectance_scale', 'roll_angle', 'sat_id',
-             'solar_azimuth_angle', 'solar_elevation_angle', 'sw_version', 'terrain_correction', 'tile_id']
+             'fill_fraction', 'geolocation_accuracy', 'geometry', 'geotrans', 'id', 'identifier', 'key', 
+             'processed', 'product', 'projcs', 'published', 'raster_size', 'reflectance_scale', 'roll_angle', 
+             'sat_id', 'solar_azimuth_angle', 'solar_elevation_angle', 'sw_version', 'terrain_correction', 
+             'tile_id']
         """
         r = self.session.get('%s/get/%s' % (self.url, key), timeout=self.TIMEOUT)
 
