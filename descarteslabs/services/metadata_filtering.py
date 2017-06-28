ceb313f051fb7eb7730e9a2cf7f7a72cc81f1db0
Kornelijus Survila <kornelijus@descarteslabs.com>
2017-06-28 17:18:55 -0600

    Add product to the allowed fields

--- descarteslabs/services/metadata_filtering.py
+++ descarteslabs/services/metadata_filtering.py
@@ -141,7 +141,7 @@ class Fields(object):
 fields = Fields("absolute_orbit", "acquired", "archived", "area", "azimuth_angle",
                 "bright_fraction", "cirrus_fraction", "cloud_fraction", "cloud_fraction_0",
                 "cloud_fraction_1", "descartes_version", "earth_sun_distance",
-                "fill_fraction", "geolocation_accuracy", "opaque_fraction", "processed",
+                "fill_fraction", "geolocation_accuracy", "opaque_fraction", "product", "processed",
                 "published", "relative_orbit", "roll_angle", "sat_id", "solar_azimuth_angle",
                 "solar_azimuth_angle_0", "solar_elevation_angle", "solar_elevation_angle_0",
                 "sw_version", "terrain_correction", "tile_id",
