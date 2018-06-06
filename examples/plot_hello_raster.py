0a18cbf3c751d37996185e4d258942eff1befa33
Justin Poehnelt <justin.poehnelt@gmail.com>
2018-06-06 11:13:55 -0600

    include examples dir (#1485)
    GitOrigin-RevId: 4188e8cc0167ee3bb0fdf7a4a586ea8db41b48ce

---
+++ examples/plot_hello_raster.py
@@ -0,0 +1,61 @@
+"""
+==================================================
+Raster Example
+==================================================
+
+This example makes a simple request to our raster service
+and displays the image.
+
+"""
+
+from pprint import pprint
+
+import descarteslabs as dl
+import matplotlib.pyplot as plt
+
+aoi = {
+    "type": "Polygon",
+    "coordinates": [
+        [
+            [
+                -105.86975097656249,
+                36.94550173495345
+            ],
+            [
+                -104.930419921875,
+                36.94550173495345
+            ],
+            [
+                -104.930419921875,
+                37.70120736474139
+            ],
+            [
+                -105.86975097656249,
+                37.70120736474139
+            ],
+            [
+                -105.86975097656249,
+                36.94550173495345
+            ]
+        ]
+    ]
+}
+
+img, meta = dl.raster.ndarray(
+    'landsat:LC08:PRE:TOAR:meta_LC80330342017072_v1',
+    bands=['swir1', 'swir2', 'nir', 'alpha'],
+    scales=[[0, 4000], [0, 4000], [0, 4000], None],
+    data_type='Byte',
+    cutline=aoi,
+    resolution=120
+)
+
+# visualize the image
+plt.figure(figsize=[8, 8])
+plt.axis('off')
+plt.imshow(img)
+plt.show()
+
+##############################
+# We can also view the metadata returned by raster.
+pprint(meta)
---
+++ examples/raster_mosaic.py
@@ -0,0 +1,142 @@
+"""
+===============================
+Mosaicking imagery with Raster
+===============================
+
+Most often, our area of interest (AOI) does not conform the arbitrary boundaries
+of an image as collected by the satellite. The Raster API enables us to
+retrieve imagery mosaicked across our AOI. This example illustrates how Raster
+mosaics imagery, and how we can format our call to Raster to group images
+by acquisition date (and/or any other metadata property).
+
+
+"""
+import matplotlib.pyplot as plt
+
+import descarteslabs as dl
+
+print(__doc__)
+
+# Define my area of interest
+tile = dl.raster.dltile_from_latlon(lat=38.8664364,
+                                    lon=-107.238606300,
+                                    resolution=20.0,
+                                    tilesize=1024,
+                                    pad=0)
+
+# Search for Sentinel-2 imagery collected between
+# August 13 - August 21, 2017 over the AOI
+metadata = dl.metadata.search(product='sentinel-2:L1C',
+                              start_time='2017-08-13',
+                              end_time='2017-08-21',
+                              dltile=tile)
+
+################################################
+# Let's first visualize each of these image acquisitions separately.
+
+# Retrieve each image from Raster separately
+# and store the ndarrays in a list
+
+images = list()
+for feature in metadata['features']:
+    image_id = feature['id']
+    arr, meta = dl.raster.ndarray(image_id,
+                                  dltile=tile,
+                                  bands=['nir', 'red', 'green', 'alpha'],
+                                  scales=[[0, 6000, 0, 255], [0, 4000, 0, 255], [0, 4000, 0, 255]],
+                                  data_type='Byte')
+    images.append(arr)
+
+# Plot using Matplotlib
+fig = plt.figure(figsize=[6, 6])
+for i, image in enumerate(images):
+    # get acquisition date of the image
+    date = metadata['features'][i]['properties']['acquired'][:10]
+
+    # plot the image
+    ax = fig.add_subplot(2, 2, i + 1)
+    ax.imshow(image)
+    ax.set_title(date, fontsize=8)
+    plt.axis('off')
+
+plt.tight_layout()
+
+################################################
+
+# We can see that our area of interest straddles multiple
+# Sentinel-2 granules, which is why we see only partial coverage of our AOI
+# in each image. From the acquisition dates, we can see that
+# these fours images were actually acquired on only one of two dates, August 13
+# and August 20, 2017. Instead of obtaining each image individually, we
+# may instead want to group these by their acquisition date, and mosaic
+# the images acquired on the same date.
+
+
+################################################
+
+# get list of all unique imagery dates
+dates = list(set([feature['properties']['acquired'][:10] for feature in metadata['features']]))
+
+# mosaic the images acquired on each date
+images = list()
+for date in dates:
+    # get ids
+    ids = [feature['id'] for feature in metadata['features'] if feature['properties']['acquired'][:10] == date]
+    # retrieve mosaicked data
+    arr, meta = dl.raster.ndarray(ids,
+                                  dltile=tile,
+                                  bands=['nir', 'red', 'green', 'alpha'],
+                                  scales=[[0, 6000, 0, 255], [0, 4000, 0, 255], [0, 4000, 0, 255]],
+                                  data_type='Byte')
+    images.append(arr)
+
+# plot the mosaics
+fig = plt.figure(figsize=[10, 5])
+for i, image in enumerate(images):
+    date = metadata['features'][i]['properties']['acquired'][:10]
+    ax = fig.add_subplot(1, 2, i + 1)
+    ax.imshow(image)
+    ax.set_title(dates[i], fontsize=8)
+    plt.axis('off')
+
+plt.tight_layout()
+################################################
+
+################################################
+
+# Raster will mosaic the imagery in the order in which
+# they appear in the list of image IDs. Thus, if we instead
+# passed all four image IDs to raster in the order in which
+# they were returned by Metadata, it will return
+# the latest image.
+
+all_ids = [feature['id'] for feature in metadata['features']]
+arr, meta = dl.raster.ndarray(all_ids,
+                              dltile=tile,
+                              bands=['nir', 'red', 'green', 'alpha'],
+                              scales=[[0, 6000, 0, 255], [0, 4000, 0, 255], [0, 4000, 0, 255]],
+                              data_type='Byte'
+                              )
+# plot the image
+plt.figure(figsize=(6, 6))
+plt.imshow(arr)
+plt.axis('off')
+
+################################################
+
+# Now, let's reverse the order of the list of image
+# IDs, and pass that reversed list to Raster
+
+all_ids_reversed = sorted(all_ids, reverse=True)
+arr, meta = dl.raster.ndarray(all_ids_reversed,
+                              dltile=tile,
+                              bands=['nir', 'red', 'green', 'alpha'],
+                              scales=[[0, 6000, 0, 255], [0, 4000, 0, 255], [0, 4000, 0, 255]],
+                              data_type='Byte'
+                              )
+# plot the image
+plt.figure(figsize=(6, 6))
+plt.imshow(arr)
+plt.axis('off')
+
+################################################
