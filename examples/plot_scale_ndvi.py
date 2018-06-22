3f5989bdabedd697ec652f17bc9c0d72cee75d2e
Karla <37315028+karla-descartes@users.noreply.github.com>
2018-06-22 10:26:37 -0600

    Docs/update (#1662)
    GitOrigin-RevId: 49eec8e08c9e226e0fc5433a2466f8c3d06f6e1a

---
+++ examples/plot_scale_ndvi.py
@@ -0,0 +1,73 @@
+"""
+==================================================
+Scale NDVI
+==================================================
+
+This example shows how to request MODIS NDVI data
+and scale the returned values.
+
+"""
+
+import descarteslabs as dl
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# A bounding box geometry
+aoi = {
+    "type": "Polygon",
+    "coordinates": [
+        [
+            [
+                -105.86975097656249,
+                36.94550173495345
+            ],
+            [
+                -104.930419921875,
+                36.94550173495345
+            ],
+            [
+                -104.930419921875,
+                37.70120736474139
+            ],
+            [
+                -105.86975097656249,
+                37.70120736474139
+            ],
+            [
+                -105.86975097656249,
+                36.94550173495345
+            ]
+        ]
+    ]
+}
+
+
+# Request Modis imagery, which contains indicies that need to be scaled
+fc = dl.metadata.search(products="modis:09:CREFL", geom=aoi, start_time="2017-05-01",
+                        end_time="2018-05-15", limit=1)
+
+# Fetch the band information using the Metadata API, including the NDVI ranges
+band_info = dl.metadata.get_bands_by_product("modis:09:CREFL")
+physical_range = band_info['derived:ndvi']['physical_range']
+valid_range = band_info['derived:ndvi']['data_range']
+
+
+# Isolate the image IDs to pull data for
+feat_ids = [feat['id'] for feat in fc['features']]
+
+
+# Request the NDVI band and scale it accordingly and the alpha band for masking next
+arr, meta = dl.raster.ndarray(
+    feat_ids,
+    cutline=aoi,
+    bands=['ndvi', 'alpha'],
+    scales=[[valid_range[0], valid_range[1], physical_range[0], physical_range[1]], None],
+    data_type='Float32',
+    resolution=120)
+
+# mask out nodata pixels
+nodata = arr[:, :, -1] == 0
+masked = np.where(nodata, 0, arr[:, :, 0])
+
+plt.hist(masked)
---
+++ examples/save_geotiff.py
@@ -0,0 +1,55 @@
+"""
+==================================================
+Export Image
+==================================================
+
+This example demonstrates how to save an image
+to your local machine in GeoTiff format.
+"""
+
+import descarteslabs as dl
+
+# Create an aoi feature to clip imagery to
+box = {
+    "type": "Polygon",
+    "coordinates": [
+        [
+            [
+                -108.64292971398066,
+                33.58051349561343
+            ],
+            [
+                -108.27082685426221,
+                33.58051349561343
+            ],
+            [
+                -108.27082685426221,
+                33.83925599538719
+            ],
+            [
+                -108.64292971398066,
+                33.83925599538719
+            ],
+            [
+                -108.64292971398066,
+                33.58051349561343
+            ]
+        ]
+    ]
+}
+
+# Two predefined image IDs for mosaic and download. These can be obtained through a Metadata or Scenes API search
+images = ['landsat:LC08:01:RT:TOAR:meta_LC08_L1TP_035037_20180602_20180602_01_RT_v1',
+          'landsat:LC08:01:RT:TOAR:meta_LC08_L1TP_035036_20180602_20180602_01_RT_v1']
+
+# The Raster API call to download an image mosaic. Other parameters are available
+# The file is written in to the same directory as the script.
+dl.raster.raster(
+    inputs=images,
+    bands=['red', 'green', 'blue', 'alpha'],
+    scales=[[0, 5500], [0, 5500], [0, 5500], None],
+    data_type='Byte',
+    cutline=box,
+    save=True,
+    outfile_basename='save_local',
+    resolution=60)
