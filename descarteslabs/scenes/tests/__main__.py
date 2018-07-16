7ae0412955673c816e54c951e3eabd5886211d57
Justin Poehnelt <justin.poehnelt@gmail.com>
2018-07-16 14:26:44 -0600

    add coverage info (#1891)
    GitOrigin-RevId: 1d0881f0c1a2617012ffef469650e40390428611

--- descarteslabs/scenes/tests/__main__.py
+++ descarteslabs/scenes/tests/__main__.py
@@ -2,5 +2,8 @@ import nose
 import os
 from nose.loader import TestLoader
 
+os.environ['NOSE_WITH_COVERAGE'] = 'True'
+os.environ['NOSE_COVER_PACKAGE'] = 'descarteslabs.scenes'
+
 nose.main(testLoader=TestLoader(workingDir=os.path.split(os.path.abspath(__file__))[0]), argv=["", "--nocapture"])
 # argv parameter allows stdout to pass through to bazel
