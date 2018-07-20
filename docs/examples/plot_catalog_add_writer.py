3a3b037bb3009cb721d77250c8d71a7ca5dce7ef
Justin Poehnelt <justin.poehnelt@gmail.com>
2018-07-20 11:24:17 -0600

    move to correct location and remove try(#1964)
    GitOrigin-RevId: 58005d6940b7a66cffb1540cfb121f3c38a554cb

---
+++ docs/examples/plot_catalog_add_writer.py
@@ -0,0 +1,48 @@
+"""
+==================================================
+Catalog Product Writers Example
+==================================================
+
+This example adds an additional writer to a product.
+
+"""
+
+from descarteslabs.client.services import Catalog
+import uuid
+
+
+catalog = Catalog()
+
+
+product_id = str(uuid.uuid4())[:8]
+
+product = catalog.add_product(
+    product_id=product_id,
+    title='My Product',
+    description='This is my product.'
+)
+print('Product {} added.'.format(product['data']['id']))
+
+product_attrs = product['data']['attributes']
+
+##############################
+# The product that we just created doesn't have any writers.
+print('Product writers: {}'.format(product_attrs['writers']))
+
+##############################
+# However, we can add a writer to this product.
+changed_product = catalog.change_product(
+    product_id=product['data']['id'],
+    writers=['email:someuser@gmail.com']  # Could be the user hash, email, group, or org of another user.
+)
+changed_product_attrs = changed_product['data']['attributes']
+
+##############################
+# Now, ``'user:some_user_hash'`` is a writer for this product. This user can now change the product metadata,
+# add bands, and add imagery to this product.
+print('Changed product writers: {}'.format(changed_product_attrs['writers']))
+
+#############################
+# Delete our product, we don't need it anymore.
+catalog.remove_product(product_id, add_namespace=True)
+print('Product removed.')
