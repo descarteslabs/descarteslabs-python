f9fbbdda3463619e32a0d1b7fad7befa88436a78
Gabe Joseph <gjoseph92@gmail.com>
2018-09-24 09:16:54 -0600

    Setup.py: use a static rST `long_description` to look better on PyPI (#2633)
    GitOrigin-RevId: e9cb0b6bea7870f263c3673784f5542c9df18c65

--- setup.py
+++ setup.py
@@ -13,6 +13,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Descartes Labs Python Client
+
+.. code-block:: bash
+
+    pip install descarteslabs[complete]
+
+Documentation is available at https://docs.descarteslabs.com.
+
+Source code and version information is at https://github.com/descarteslabs/descarteslabs-python.
+
+The Descartes Labs Platform simplifies analysis of **global-scale raster data** by providing:
+
+  * Access to a catalog of petabytes of disparate geospatial data,
+    all normalized and interoperable through one **common interface**
+  * **Infrastructure** to parallelize any code across thousands of machines co-located with that data
+  * The ability to **add new data to that catalog**—whether the output of analysis on existing data,
+    or from a proprietary source—which can then be used as an input for more analysis
+  * A Python client library to access these systems
+  * Web interfaces to `browse this catalog <https://catalog.descarteslabs.com/>`_
+    and `view imagery <https://viewer.descarteslabs.com/>`_, including your data you create
+"""
 
 import ast
 import os
@@ -20,6 +41,8 @@ import re
 import sys
 
 from setuptools import find_packages, setup
+
+DOCLINES = __doc__.split("\n")
 
 # Parse version out of descarteslabs/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -44,8 +67,8 @@ def do_setup():
 
     kwargs = {}
     kwargs['name'] = 'descarteslabs'
-    kwargs['description'] = 'Descartes Labs Python Library'
-    kwargs['long_description'] = open('README.md').read()
+    kwargs['description'] = DOCLINES[0]
+    kwargs['long_description'] = "\n".join(DOCLINES[2:])
     kwargs['author'] = 'Descartes Labs'
     kwargs['author_email'] = 'hello@descarteslabs.com'
     kwargs['url'] = 'https://github.com/descarteslabs/descarteslabs-python'
@@ -65,8 +88,8 @@ def do_setup():
     kwargs['packages'] = find_packages('.')
     kwargs['entry_points'] = {
         'console_scripts': [
-            'descarteslabs = descarteslabs.client.scripts.__main__:main'
-        ]
+            'descarteslabs = descarteslabs.client.scripts.__main__:main',
+        ],
     }
     kwargs['install_requires'] = [
         "cloudpickle==0.4.0",
@@ -88,8 +111,8 @@ def do_setup():
         "complete": [
             'blosc;platform_system!="Windows"',
             "numpy>=1.10.0",
-            "matplotlib>=2.1.0"
-        ]
+            "matplotlib>=2.1.0",
+        ],
     }
     kwargs['license'] = 'Apache 2.0'
     kwargs['zip_safe'] = False
