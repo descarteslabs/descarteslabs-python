afeeacc8db1e5d07f7db7bdfe3aa9767474fa399
Justin Poehnelt <justin.poehnelt@gmail.com>
2018-10-10 16:42:18 -0600

    python client: fix non ascii chars in setup.py (#2744)
    GitOrigin-RevId: 4c7fe18a2d0c0072cd39ec4641af179b2eb15bf7

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ The Descartes Labs Platform simplifies analysis of **global-scale raster data** 
   * Access to a catalog of petabytes of disparate geospatial data,
     all normalized and interoperable through one **common interface**
   * **Infrastructure** to parallelize any code across thousands of machines co-located with that data
-  * The ability to **add new data to that catalog**—whether the output of analysis on existing data,
-    or from a proprietary source—which can then be used as an input for more analysis
+  * The ability to **add new data to that catalog**-whether the output of analysis on existing data,
+    or from a proprietary source-which can then be used as an input for more analysis
   * A Python client library to access these systems
   * Web interfaces to `browse this catalog <https://catalog.descarteslabs.com/>`_
     and `view imagery <https://viewer.descarteslabs.com/>`_, including your data you create
