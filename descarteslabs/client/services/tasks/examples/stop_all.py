dd211349513157a8e12de07d289489e995c6e525
Justin Poehnelt <justin.poehnelt@gmail.com>
2019-03-08 08:27:34 -0700

    [EP-786] remove old examples (#3933)
    GitOrigin-RevId: ff77401350c709c686a217894bf5bfb61afe9507

--- descarteslabs/client/services/tasks/examples/stop_all.py
+++
@@ -1,6 +0,0 @@
-from descarteslabs.client.services.tasks import Tasks
-
-at = Tasks()
-for g in at.iter_groups(status='running'):
-    print("Stopping {name}".format(**g))
-    at.terminate_group(g.id)
