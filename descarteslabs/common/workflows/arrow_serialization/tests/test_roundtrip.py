a6ba948f41ba3cef064030a283f7500aacd60465
Gabe Joseph <gabe@descarteslabs.com>
2020-03-20 09:55:39 -0600

    Reactivate arrow serialization e2e test (#6574)
    GitOrigin-RevId: 1055ba722a4d4ce7fd3384dc388de1f459076619

--- descarteslabs/common/workflows/arrow_serialization/tests/test_roundtrip.py
+++ descarteslabs/common/workflows/arrow_serialization/tests/test_roundtrip.py
@@ -1,5 +1,4 @@
-import pytest
-from hypothesis import given
+from hypothesis import given, note, assume
 import hypothesis.strategies as st
 import hypothesis.extra.numpy as st_np
 
@@ -9,18 +8,17 @@ import pyarrow as pa
 from ..context import serialization_context
 
 
-# Fails, most concerningly, on:
-# array([1], dtype=uint16)
-# array([256], dtype=uint16)
-
-
-@pytest.mark.xfail
 @given(
     st_np.arrays(
         dtype=st_np.scalar_dtypes(), shape=st.one_of(st.just(0), st_np.array_shapes())
     )
 )
 def test_plain_roundtrip(arr):
+    assume(arr.dtype.byteorder in ("=", "<"))
+    # https://issues.apache.org/jira/browse/ARROW-4677
+    # Otherwise this fails on endianness changes, such as:
+    # array([1], dtype=">i2") -> array([256], dtype="int16")
+
     cereal = pa.serialize(arr, context=serialization_context)
     buf = cereal.to_buffer()
     decereal = pa.deserialize(buf, context=serialization_context)
@@ -28,4 +26,5 @@ def test_plain_roundtrip(arr):
     assert arr.dtype == decereal.dtype.newbyteorder(
         arr.dtype.byteorder
     )  # arrow may change endianness
+    note((arr.dtype, decereal.dtype))
     np.testing.assert_array_equal(arr, decereal)
