01dd1814f0569d19af86db9575e45eaff245ac91
stephaniegott <stephanie.gott@descarteslabs.com>
2020-03-26 15:55:20 -0600

    testing

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def do_setup():
         version=version,
         packages=find_packages(),
         package_data={'descarteslabs': ["client/services/tasks/tests/data/dl_test_package/*.json",
-                                        "client/services/tasks/tests/data/dl_test_package/*.txt"]},
+                                        "client/services/tasks/tests/data/*.txt"]},
         include_package_data=True,
         entry_points={
             "console_scripts": [
