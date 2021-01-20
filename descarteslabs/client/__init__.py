c70cc884b9512c71b66d0266e4a1505c2efce7ad
Stephen C. Pope <stephen@descarteslabs.com>
2021-01-20 12:25:35 -0700

    Client: Check/deprecate python version [PLAT-512] (#7962)
    GitOrigin-RevId: c52fe10458d8ba501279292c94c30397daef932a

--- descarteslabs/client/__init__.py
+++ descarteslabs/client/__init__.py
@@ -11,3 +11,24 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import sys
+import warnings
+
+if sys.version_info < (3, 6):
+    msg = "Python version {}.{} not supported by descarteslabs.client".format(
+        sys.version_info.major, sys.version_info.minor
+    )
+    raise ImportError(msg)
+
+if sys.version_info < (3, 7):
+    msg = "Support for Python version {}.{} has been deprecated and will be removed in a future version.".format(
+        sys.version_info.major, sys.version_info.minor
+    )
+    warnings.warn(msg, FutureWarning)
+
+if sys.version_info >= (3, 9):
+    msg = "Python version {}.{} is not supported yet. You may encounter unexpected errors.".format(
+        sys.version_info.major, sys.version_info.minor
+    )
+    warnings.warn(msg, FutureWarning)
