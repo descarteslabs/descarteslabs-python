f303eb1b8ee39c361afc6c7155964de381dd741c
Stephen C. Pope <stephen@descarteslabs.com>
2024-02-07 18:46:03 -0700

    Vector: Fix vector visualize (#12443)
    GitOrigin-RevId: 9c0a311e6fdbcef16cb723389db3acfcfc5eb01e

--- descarteslabs/core/vector/layers.py
+++ descarteslabs/core/vector/layers.py
@@ -2,7 +2,12 @@ try:
     from ipyleaflet import VectorTileLayer
     from traitlets import Dict
 except ImportError:
-    VectorTileLayer = object
+    raise ImportError(
+        "The visualization support in the `descarteslabs.vector` Python package"
+        " requires the `viz` extra to be installed."
+        " Please run `pip install descarteslabs[viz]` and try again."
+        " Alternatively you can install the `ipyleaflet` package directly."
+    )
 
 
 class DLVectorTileLayer(VectorTileLayer):
@@ -10,12 +15,4 @@ class DLVectorTileLayer(VectorTileLayer):
     A minimal wrapper around VectorTileLayer to add fetch_options
     """
 
-    def __init__(self, *args, **kwargs):
-        if VectorTileLayer is object:
-            raise ImportError(
-                "The visualization support in the `descarteslabs.vector` Python package"
-                " requires the `viz` extra to be installed."
-                " Please run `pip install descarteslabs[viz]` and try again."
-            )
-        super(DLVectorTileLayer, self).__init__(*args, **kwargs)
-        self.fetch_options = Dict({"credentials": "include"}).tag(sync=True, o=True)
+    fetch_options = Dict({"credentials": "include"}).tag(sync=True, o=True)
